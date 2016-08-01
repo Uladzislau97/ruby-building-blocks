2819e503a9653ecc0828f0b63a6e0bcf4a3809ff
Uladzislau97 <VSvasilyev97@gmail.com>
2016-08-01 15:03:32 +0300

    Add bubble_sort_by method

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -12,4 +12,19 @@ def bubble_sort(array)
   array
 end
 
-p bubble_sort([5, 4, 3, 2, 1, 0])
+def bubble_sort_by(array)
+  n = array.length
+  
+  (n - 1).times do
+    for i in (0...n-1)
+  	  if yield(array[i], array[i + 1]) > 0
+  	    array[i], array[i + 1] = array[i + 1], array[i]
+  	  end
+    end
+  end
+
+  array
+end
+
+p bubble_sort([4,3,78,2,0,2])
+p bubble_sort_by(["hi","hello","hey"]) { |left, right| right.length - left.length }
