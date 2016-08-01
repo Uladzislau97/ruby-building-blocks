8949ef38e3d6a99968b19902ab1d249f237c8c0c
Uladzislau97 <VSvasilyev97@gmail.com>
2016-08-01 15:22:36 +0300

    Add my_each_with_index method

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -2,8 +2,18 @@ module Enumerable
   def my_each
     return self unless block_given?
 
-    for i in self
-      yield(i)
+    for element in self
+      yield(element)
+    end
+  end
+
+  def my_each_with_index
+ 	return self unless block_given?
+
+ 	index = 0
+    for element in self
+      yield(element, index)
+      index += 1
     end
   end
 end
@@ -11,3 +21,8 @@ end
 (1..5).my_each do |element|
   puts element
 end
+
+(1..5).my_each_with_index do |element, index|
+  puts "#{index}-#{element}"
+end
+
